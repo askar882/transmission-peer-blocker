b4afa3a4aa26c19d02ebfbd7d0e31e7fff4fef32
askar882 <17901680+askar882@users.noreply.github.com>
2024-03-06 02:12:49 +0800

    Add script; Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # transmission-peer-blocker
-Bash script for blocking bad peers in transmission.
+
+English | [中文](README_cn.md)
+
+## Description
+
+Bash script for blocking bad peers in transmission with `iptables` and `ipset`.
+
+## Requirements
+
+- `transmission` is installed and running
+- `transmission-remote` command is available
+- `iptables` is available
+- `ipset` package is installed
+- `cronie` package is installed (Optional)
+
+## Usage
+
+This script can be ran manually or you can create a scheduled task with tools like `cronie`.
+
+### 1. Download the Project
+
+1. Either download the `transmission-peer-blocker.sh` file only or clone this repo to your Linux machine with `git clone https://github.com/askar882/transmission-peer-blocker` command.
+2. Grant execute permission to the script with `chmod +x transmission-peer-blocker.sh`.
+
+### 2. Create a Scheduled Task (Optional)
+
+Create a scheduled task for root user with `sudo crontab -e` command and paste the following:
+
+```crontab
+*/5 * * * * /home/user/transmission-peer-blocker/transmission-peer-blocker.sh
+```
+
+> :memo: `cronie` executes `/home/user/transmission-peer-blocker/transmission-peer-blocker.sh` every 5 minutes. You can create your own scheduled task expression on [Crontab.guru](https://crontab.guru/). Don't forget to change the scripts path to the real path of it on your machine.
+
+## References
+
+[Transmission 屏蔽迅雷反吸血脚本](https://zhuanlan.zhihu.com/p/158711236)
+
+[session.cpp](https://github.com/c0re100/qBittorrent-Enhanced-Edition/blob/v4_2_x/src/base/bittorrent/session.cpp#L2212)
---
+++ transmission-peer-blocker.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+#
+# Block peers using Xunlei, BT players and unknown clients for transmission.
+#
+# Reference: [Transmission 屏蔽迅雷反吸血脚本](https://zhuanlan.zhihu.com/p/158711236)
+#   Xunlei client identifiers: https://github.com/c0re100/qBittorrent-Enhanced-Edition/blob/v4_2_x/src/base/bittorrent/session.cpp#L2212
+#   Transmission Blocklist: https://github.com/transmission/transmission/blob/master/libtransmission/blocklist.cc
+
+readonly LOGGER_TAG='transmission-peer-blocker'
+readonly ID_FILTER='\-\(\(XL\|SD\|XF\|QD\|BN\|DL\|TS\|LT\|SP\)\([0-9]\+\)\|\(UW\w\{4\}\)\)-\|Xunlei\|Unknown'
+readonly IPSET='trans'
+readonly IPSET6='trans6'
+
+
+###################################################
+# Main function. Block IPs matching specific clients.
+# Globals:
+#   ID_FILTER
+#   IPSET
+# Arguments:
+#   None
+# Outputs:
+#   None
+###################################################
+function main() {
+  log 'Start scanning peers for illegal clients.'
+  peers_info="$(query_peers | grep "${ID_FILTER}")"
+  if [[ -z "${peers_info}" ]]; then
+    log 'No illegal clients found. Exit.'
+    return
+  fi
+  # Initialize if IP set doesn't exist.
+  ipset list "${IPSET}" >/dev/null 2>&1
+  if [[ "$?" != "0" ]]; then
+    init
+  fi
+  echo "${peers_info}" | while read info; do
+    ip="$(echo "${info}" | cut -d' ' -f1)"
+    block_ip "${ip}"
+  done
+  log 'Peer blocking done.'
+}
+
+#######################################################
+# Query all peers for their IPs and client identifiers.
+# Arguments:
+#   None
+# Outputs:
+#   All peers' IPs and client identifiers.
+#######################################################
+function query_peers() {
+  # `transmission-remote -t all -ip` writes in the following format for every torrent:
+  ## Address                                   Flags         Done  Down    Up      Client
+  ## 93.43.200.6                               TE            100.0    0.0     0.0  qBittorrent 4.3.8
+
+  # Query IPs and client identifiers exluding blank lines and lines starting with 'Address'.
+  result="$(transmission-remote -t all -ip | grep -v '^Address\|^$')"
+  echo "${result}"
+}
+
+########################################################
+# Add IP address to ipset. Works both for IPv4 and IPv6.
+# Globals:
+#   IPSET
+#   IPSET6
+# Arguments:
+#   address
+# Outputs:
+#   None
+########################################################
+function block_ip() {
+  local setname="${IPSET}"
+  if [[ "$1" =~ ':' ]]; then
+    setname="${IPSET6}"
+  fi
+  if [[ "$(ipset list ${setname})" =~ "$1" ]]; then
+    log "Address '$1' is already in blacklist."
+    return
+  fi
+  ipset add "${setname}" "$1"
+  log "Added '$1' to blacklist."
+}
+
+
+########################################
+# Log
+# Globals:
+#   LOGGER_TAG
+# Arguments:
+#   Message to be logged.
+# Outputs:
+#   None
+########################################
+function log() {
+  logger -t "${LOGGER_TAG}" "$*"
+}
+
+######################################
+# Initialize ipset and iptables rules.
+# Globals:
+#   IPSET
+#   IPSET6
+# Arguments:
+#   None
+# Outputs:
+#   None
+######################################
+function init() {
+  log 'Initializing IP set.'
+  ipset create "${IPSET}" hash:ip
+  ipset create "${IPSET6}" hash:ip family inet6
+  iptables -I INPUT -m set --match-set "${IPSET}" src,dst -j DROP
+  ip6tables -I INPUT -m set --match-set "${IPSET6}" src,dst -j DROP
+}
+
+
+main
